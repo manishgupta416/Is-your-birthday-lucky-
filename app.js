76a6f91cba85f2741c81326521151d10bfc323cb
Manish Gupta <manish.info2020@gmail.com>
2022-07-03 17:57:08 +0530

    added logic and some styling

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const dateOfBirth = document.querySelector("#input-date")
+const luckyNumber = document.querySelector("#input-number")
+const showOutput = document.querySelector(".output")
+const btnCheck = document.querySelector("#check-btn")
+
+// console.log(dateOfBirth ,luckyNumber , showOutput)
+
+btnCheck.addEventListener('click' ,  checkBirthdayDateIsLucky)
+
+function checkBirthdayDateIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob)
+    console.log(sum)
+    if(sum && dob) {
+        compareValues(sum , luckyNumber.value)
+    }else{
+        showOutput.innerText="Please Enter both fields"
+    }
+   
+}
+
+function compareValues(sum , luckyNumber) {
+    if(sum % luckyNumber == 0) {
+        showOutput.innerText = "your birthday is lucky 🥳🚀"
+    } else{
+        showOutput.innerText = "your birthday is not lucky😕"
+    }
+}
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-","")
+    let sum=0;
+
+    for (let i = 0; i < dob.length; i++) {
+        sum = sum + Number(dob.charAt(i))
+        
+    }
+    return sum;
+}
